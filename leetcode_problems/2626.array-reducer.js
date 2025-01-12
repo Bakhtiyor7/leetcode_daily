b28b65db4af098cff1fc84741fdb772e3776ab66
Bakhtiyor7 <baxtiyor.axatov77@gmail.com>
2025-01-12 20:23:28 +0900

    fix: update 2626 array.reduce

--- leetcode_problems/2626.array-reducer.js
+++ leetcode_problems/2626.array-reducer.js
@@ -1,15 +1,4 @@
 /**
- * Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
- *
- * This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
- *
- * If the length of the array is 0, the function should return init.
- *
- * Please solve it without using the built-in Array.reduce method.
- *
- *
- *
- *
  * @param {number[]} nums
  * @param {Function} fn
  * @param {number} init
@@ -17,24 +6,17 @@
  */
 
 const numbers = [1, 2, 3, 4];
-let initVal = 10;
+const func = function sum(accum, curr) {
+  return accum + curr;
+};
+let init = 100;
 
-const reducer = function (arr, val) {
-  let res = [];
-  let n;
-  console.log(n);
-  for (let i = 0; i < arr.length; i++) {
-    n = val + arr[0];
-    res.push(arr[i] + n);
+var reduce = function (nums, fn, init) {
+  let val = init;
+  for (let i = 0; i < nums.length; i++) {
+    val = fn(val, nums[i]);
   }
-
-  console.log(res);
-  arr.forEach((num) => {
-    initVal += num;
-  });
-  console.log(initVal); // Output: 10
+  return val;
 };
 
-var reduce = function (nums, fn, init) {};
-
-reduce(numbers, reducer(numbers, initVal), initVal);
+reduce(numbers, func, init);
