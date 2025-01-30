4958218e207aeaf72d879c33b70f2ed712cf659c
Bakhtiyor7 <baxtiyor.axatov77@gmail.com>
2025-01-31 00:27:10 +0900

    refactor: (isStringPalindrome.js) edit if else statement to be more understandable

--- beginner_freindly/isStringPalindrome.js
+++ beginner_freindly/isStringPalindrome.js
@@ -3,13 +3,21 @@
 // 💡 Problem: A string is a palindrome if it reads the same forward and backward. Write a function to check if a string is a palindrome.
 
 function isPalindrome(str) {
-  let split = str.split("");
-  let reverse = split.reverse();
-  let join = reverse.join("");
+  let arrVal = str.split("");
+  let reverseString = arrVal.reverse();
+  let result = reverseString.join("");
 
-  return !!(str == join);
+  if (str == result) {
+    console.log("string is a palindrome");
+    return true;
+  } else {
+    console.log("string is NOT a palindrome");
+    return false;
+  }
+  // return !!(str == join); // simplified if else
   // returns true if palindrome, or false if not.
 }
 
-console.log(isPalindrome("racecar")); // Output: true
-console.log(isPalindrome("abcdcba")); // Output: false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); //  false
+console.log(isPalindrome("wow")); // true
