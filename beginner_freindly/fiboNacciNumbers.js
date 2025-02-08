6c7299eae3bb53648696722adb9d15e2aa6f197f
Bakhtiyor7 <baxtiyor.axatov77@gmail.com>
2025-02-08 18:30:54 +0900

    feat: (beginner) fibonacciNumbers solved

---
+++ beginner_freindly/fiboNacciNumbers.js
@@ -0,0 +1,12 @@
+let prev2 = 0;
+let prev1 = 1;
+
+console.log(prev2);
+console.log(prev1);
+
+for (let fibo = 0; fibo < 18; fibo++) {
+  let newFibo = prev1 + prev2;
+  console.log(newFibo);
+  prev2 = prev1;
+  prev1 = newFibo;
+}
