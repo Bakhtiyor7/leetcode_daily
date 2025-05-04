54c014ca06a098b2d2c1df17380e77bcafb1d525
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-05-04 19:07:55 +0900

    feat: neetcode -> two sum solved

---
+++ neetcode/arrays/twoSum.js
@@ -0,0 +1,29 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums, target) {
+    // if the integer equals the target finish the job directly
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) {
+        return [i];
+      }
+    }
+
+    // nested loop
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length + 1; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+    return [];
+  }
+}
+
+const solution = new Solution();
+console.log(solution.twoSum([1, 3, 4, 5], 7));
+console.log(solution.twoSum([3, 2, 8, 5], 8));
