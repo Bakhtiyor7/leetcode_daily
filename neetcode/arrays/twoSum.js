05dd7989733b2117ff5459f783a8f61518dd7a9a
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-05-18 12:55:57 +0900

    fix: twoSum - out of bound error

--- neetcode/arrays/twoSum.js
+++ neetcode/arrays/twoSum.js
@@ -7,7 +7,7 @@ class Solution {
   twoSum(nums, target) {
     // nested loop
     for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length + 1; j++) {
+      for (let j = i + 1; j < nums.length; j++) {
         if (nums[i] + nums[j] === target) {
           return [i, j];
         }
