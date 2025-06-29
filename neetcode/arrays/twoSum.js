05b4df7903d09ff5af25112672977120a2eee3d8
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-06-29 20:43:17 +0900

    feat: twosum better solution

--- neetcode/arrays/twoSum.js
+++ neetcode/arrays/twoSum.js
@@ -20,3 +20,27 @@ class Solution {
 const solution = new Solution();
 console.log(solution.twoSum([1, 3, 4, 5], 7));
 console.log(solution.twoSum([3, 2, 8, 5], 8));
+
+// Improved solution using a hash map
+// class Solution {
+//   /**
+//    * @param {number[]} nums
+//    * @param {number} target
+//    * @return {number[]}
+//    */
+//   twoSum(nums, target) {
+//     const numToIndex = {}; // or use new Map() for Map version
+//     for (let i = 0; i < nums.length; i++) {
+//       const complement = target - nums[i];
+//       if (numToIndex.hasOwnProperty(complement)) {
+//         return [numToIndex[complement], i];
+//       }
+//       numToIndex[nums[i]] = i;
+//     }
+//     return [];
+//   }
+// }
+
+// const solution = new Solution();
+// console.log(solution.twoSum([1, 3, 4, 5], 7)); // Output: [1, 2]
+// console.log(solution.twoSum([3, 2, 8, 5], 8)); // Output: [0, 3]
