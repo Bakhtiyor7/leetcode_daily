22c5110fc77f06085fbc97194119d80407edefee
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-08-02 15:58:46 +0900

    feat: (neetcode) solved threesum problem with two pointers algorithm

--- neetcode/twoPointers/3sum.js
+++ neetcode/twoPointers/3sum.js
@@ -18,27 +18,84 @@ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 */
  
 
+ //====== Brute force=========
+// time O(n3) space o(m)
+
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @return {number[][]}
+//      */
+//     threeSum(nums) {
+//         const res = new Set();
+//         nums.sort((a, b) => a - b);
+//         for (let i = 0; i < nums.length; i++) {
+//             for (let j = i + 1; j < nums.length; j++) {
+//                 for (let k = j + 1; k < nums.length; k++) {
+//                     if (nums[i] + nums[j] + nums[k] === 0) {
+//                         res.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+//                     }
+//                 }
+//             }
+//         }
+//         return Array.from(res).map(item => JSON.parse(item));
+//     }
+// }
+
+// ===============================
+
+// ======== Two Pointer=========
+// time o(n2) space o(n) or o(1)
+
 class Solution {
-    /**
-     * @param {number[]} nums
-     * @return {number[][]}
-     */
-    threeSum(nums) {
-        const res = new Set();
-        nums.sort((a, b) => a - b);
-        for (let i = 0; i < nums.length; i++) {
-            for (let j = i + 1; j < nums.length; j++) {
-                for (let k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] === 0) {
-                        res.add(JSON.stringify([nums[i], nums[j], nums[k]]));
-                    }
-                }
-            }
-        }
-        return Array.from(res).map(item => JSON.parse(item));
-    }
+
+	threeSum(nums) {
+		const count = nums.sort((a, b) => a - b) // we sort it so negative numbers are on the left
+		let res = [];
+
+		for (let i = 0; i < nums.length; i++) {
+			if (nums[i] > 0) break; // if index is bigger than 0, stop immediately;
+			if (i > 0 && nums[i] === nums[i] -1) continue; 
+
+			let l = i + 1;
+			let r = nums.length - 1;
+
+			while (l < r) {
+				const sum = nums[i] + nums[l] + nums[r];
+				if(sum > 0) {
+					r--
+				} else if (sum < 0) {
+					l++
+				} else {
+					res.push([nums[i], nums[l], nums[r]]);
+					l++
+					r--
+					while (l < r && nums[i] === nums[l - 1]) 
+						l++
+				} 
+			}
+			
+		}
+		return res;
+	}
 }
 
 const solution = new Solution();
-console.log(solution.threeSum([1, 1, 0, -1, 0, 1]));
- 
+console.log(solution.threeSum([-1, 0, 1, 2, -1, -4]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
